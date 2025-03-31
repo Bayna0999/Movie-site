64ed3182ac67a0b19d380ac5ad05871354638fe5
Bayna0999 <baynaa123455@gmail.com>
2025-03-31 22:57:03 +0800

    gerees

--- src/components/component/MovieDetails.tsx
+++ src/components/component/MovieDetails.tsx
@@ -14,7 +14,7 @@ export const MovieDetails = () => {
 
   const getMovieData = async () => {
     try {
-      const { data } = await axiosInstance.get(`movie/972533?language=en-US`);
+      const { data } = await axiosInstance.get(`movie/${params.id}?language=en-US`);
       setGetData(data);
     } catch (err: any) {
       console.log(err.message);
@@ -26,7 +26,7 @@ export const MovieDetails = () => {
       const { data } = await axiosInstance.get(
         `movie/${params.id}/similar?language=en-US`
       );
-      setGetSimilarData(data.crew);
+      setGetSimilarData(data);
       console.log(data.results, "data");
     } catch (err: any) {
       console.log(err.message);
@@ -38,15 +38,15 @@ export const MovieDetails = () => {
       const { data } = await axiosInstance.get(
         `movie/${params.id}/credits?language=en-US`
       );
-      setGetCredits(data.results);
+      setGetCredits(data);
     } catch (err: any) {
       console.log(err.message);
     }
   };
 
-  console.log(getData);
-  console.log(getSimilarData);
-  console.log(getCredits);
+  console.log(getData.poster_path," getdata");
+  // console.log(getSimilarData," getSimilardata");
+  // console.log(getCredits," getdataCredits");
 
   useEffect(() => {
     getMovieData();
@@ -58,8 +58,8 @@ export const MovieDetails = () => {
     <div className="justify-center items-center w-screen h-screen ">
       <div className="flex justify-between ">
         <div className="flex flex-col h-[68px] w-[211px]">
-          <p className="w-fulll h-10">{getData}</p>
-          <p className="w-full h-7">{getData[0]}</p>
+          <p className="w-fulll h-10">{getData.title}</p>
+          <p className="w-full h-7">{getData.release_date + " " +getData.runtime}</p>
         </div>
         <div className="flex flex-col">
           <p>rating</p>
@@ -67,17 +67,17 @@ export const MovieDetails = () => {
             <GoStarFill className="fill-yellow-400 w-7 h-7" />
             <div className="flex flex-col ">
               <div className="flex ">
-                <p>rate</p>
+                <p>{getData.vote_average}</p>
                 <p className="text-[#71717A]">/10</p>
               </div>
-              <p>views</p>
+              <p>{getData.popularity}</p>
             </div>
           </div>
         </div>
       </div>
       <div className="flex gap-5 ">
         <img
-          src={ImageUrl(getData?.poster_path)}
+          src={ImageUrl(getData.poster_path)}
           className="rounded-sm w-[290px] h-[428px]"
         />
         <img
@@ -91,7 +91,7 @@ export const MovieDetails = () => {
         <div className="w-full gap-5 h-fit">
           <div className="w-full">
             <p>Director</p>
-            <p>???</p>
+            <p>{}</p>
           </div>
           <div>
             <p>Writers</p>
@@ -105,7 +105,7 @@ export const MovieDetails = () => {
       </div>
       <div>
         <div className="w-full flex justify-between ">
-          <p className="text-[24px]">title</p>
+          <p className="text-[24px]">{getData.title}</p>
           <button className="flex items-center justify-center gap-3">
             See more <GrLinkNext />
           </button>
