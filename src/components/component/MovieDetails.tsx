c338c307eafb26e6d4603eb969a59b0dfb37cf9e
24LP6733 24LP6733 <24LP6733@G302-CC0097.local>
2025-04-01 12:35:33 +0800

    angideer

--- src/components/component/MovieDetails.tsx
+++ src/components/component/MovieDetails.tsx
@@ -4,17 +4,34 @@ import React, { use, useEffect, useState } from "react";
 import { GoStarFill } from "react-icons/go";
 import { GrLinkNext } from "react-icons/gr";
 import { useParams } from "next/navigation";
-import { axiosInstance, ImageUrl } from "@/lib/utils";
+import {
+  axiosInstance,
+  CreditDataType,
+  ImageUrl,
+  SimilarDataType,
+  SpecificDataType,
+} from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+import { Card } from "./Card";
 
 export const MovieDetails = () => {
-  const [getData, setGetData] = useState([]);
-  const [getSimilarData, setGetSimilarData] = useState([]);
-  const [getCredits, setGetCredits] = useState([]);
+  const [getData, setGetData] = useState<SpecificDataType>({
+    poster_path: "",
+    backdrop_path: "",
+  });
+  const [getSimilarData, setGetSimilarData] = useState<SimilarDataType[]>([]);
+  const [getCredits, setGetCredits] = useState<CreditDataType>({
+    id: 1,
+    cast: [],
+    crew: [],
+  });
   const params = useParams();
 
   const getMovieData = async () => {
     try {
-      const { data } = await axiosInstance.get(`movie/${params.id}?language=en-US`);
+      const { data } = await axiosInstance.get(
+        `movie/${params.id}?language=en-US`
+      );
       setGetData(data);
     } catch (err: any) {
       console.log(err.message);
@@ -26,8 +43,7 @@ export const MovieDetails = () => {
       const { data } = await axiosInstance.get(
         `movie/${params.id}/similar?language=en-US`
       );
-      setGetSimilarData(data);
-      console.log(data.results, "data");
+      setGetSimilarData(data.results);
     } catch (err: any) {
       console.log(err.message);
     }
@@ -44,71 +60,118 @@ export const MovieDetails = () => {
     }
   };
 
-  console.log(getData.poster_path," getdata");
-  // console.log(getSimilarData," getSimilardata");
-  // console.log(getCredits," getdataCredits");
+  // console.log(getData, " getdata");
+  console.log(getSimilarData, " getSimilardata");
+  // console.log(getCredits, " getdataCredits");
 
   useEffect(() => {
     getMovieData();
     getSimilarMovieData();
     getCreditsData();
   }, []);
-
+  const directors = getCredits.crew.filter((el) => el.job === "Director");
+  const Writers = getCredits.crew.filter((el) => el.job === "Writer");
+  const Stars = getCredits.crew.filter(
+    (el) => el.known_for_department === "Acting"
+  );
   return (
-    <div className="justify-center items-center w-screen h-screen ">
-      <div className="flex justify-between ">
-        <div className="flex flex-col h-[68px] w-[211px]">
-          <p className="w-fulll h-10">{getData.title}</p>
-          <p className="w-full h-7">{getData.release_date + " " +getData.runtime}</p>
-        </div>
-        <div className="flex flex-col">
-          <p>rating</p>
-          <div className="flex items-center justify-center">
-            <GoStarFill className="fill-yellow-400 w-7 h-7" />
-            <div className="flex flex-col ">
-              <div className="flex ">
-                <p>{getData.vote_average}</p>
-                <p className="text-[#71717A]">/10</p>
+    <div className=" w-screen h-screen ">
+      <div className="mx-[180px] flex flex-col gap-5 justify-center items-center ">
+        <div className="flex justify-between w-full">
+          <div className="flex flex-col h-[68px] w-[211px]">
+            <p className="w-fulll h-10">{getData.title}</p>
+            <p className="w-full h-7">
+              {getData.release_date + " " + getData.runtime}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <p>rating</p>
+            <div className="flex items-center justify-center">
+              <GoStarFill className="fill-yellow-400 w-7 h-7" />
+              <div className="flex flex-col ">
+                <div className="flex ">
+                  <p>{getData.vote_average}</p>
+                  <p className="text-[#71717A]">/10</p>
+                </div>
+                <p className="text-3 text-[#09090B]">{getData.popularity}</p>
               </div>
-              <p>{getData.popularity}</p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="flex gap-5 ">
-        <img
-          src={ImageUrl(getData.poster_path)}
-          className="rounded-sm w-[290px] h-[428px]"
-        />
-        <img
-          src={ImageUrl(getData?.backdrop_path)}
-          className=" rounded-sm w-[760px] h-[428px]"
-        />
-      </div>
-      <div className="">
-        <div className="w-full">gerne</div>
-        <div className="w-full">elphaba</div>
-        <div className="w-full gap-5 h-fit">
-          <div className="w-full">
-            <p>Director</p>
-            <p>{}</p>
+        <div className="flex gap-5 ">
+          <img
+            src={ImageUrl(getData.poster_path)}
+            className="rounded-sm w-[290px] h-[428px]"
+          />
+          <img
+            src={ImageUrl(getData?.backdrop_path)}
+            className=" rounded-sm w-[760px] h-[428px]"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <div className="w-full flex gap-5">
+            {getData.genres?.map((value: any, index: number) => {
+              return (
+                <Badge key={index} variant="outline">
+                  {value.name}
+                </Badge>
+              );
+            })}
           </div>
-          <div>
-            <p>Writers</p>
-            <p>???</p>
-          </div>
-          <div className="w-full">
-            <p>Stars</p>
-            <p>???</p>
+          <div className="w-full">{getData.overview}</div>
+          <div className="w-full gap-[30px] h-fit">
+            <div className="flex flex-col gap-2 w-full">
+              <div className="w-full flex gap-8 mt-8">
+                <p>Director</p>
+                {directors.map((element, index) => {
+                  return <p key={index}>{element.name}</p>;
+                })}
+              </div>
+              <div className=" bg-gray-400 w-full h-[1px]"></div>
+            </div>
+
+            <div className="flex flex-col gap-2 w-full mt-8">
+              <div className="flex gap-8">
+                <p>Writers</p>
+                {Writers.map((element, index) => {
+                  return <p key={index}>{element.name}</p>;
+                })}
+              </div>
+              <div className=" bg-gray-400 w-full h-[1px]"></div>
+            </div>
+            <div className="flex flex-col gap-2 w-full mt-8">
+              <div className="w-full flex gap-8">
+                <p>Stars</p>
+                {Stars.map((element, index) => {
+                  return <p key={index}>{element.name}</p>;
+                })}
+              </div>
+              <div className=" bg-gray-400 w-full h-[1px] mx-2"></div>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <div className="w-full flex justify-between ">
-          <p className="text-[24px]">{getData.title}</p>
-          <button className="flex items-center justify-center gap-3">
-            See more <GrLinkNext />
-          </button>
+        <div className="flex flex-col justify-center items-center">
+          <div className="w-full flex justify-between ">
+            <p className="text-[24px]">More like this</p>
+            <button className="flex items-center justify-center gap-3">
+              See more <GrLinkNext />
+            </button>
+          </div>
+          <div className="flex w-full h-fit gap-8 ">
+            {getSimilarData.map((value: any, index: number) => {
+              return (
+                index < 5 && (
+                  <Card
+                    key={index}
+                    jumpToDetails={value.id}
+                    image={ImageUrl(value.poster_path)}
+                    rate={value.vote_average}
+                    title={value.title}
+                  />
+                )
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,104 @@ export const axiosInstance = axios.create({
 export const ImageUrl = (path: string) => {
   return `https://image.tmdb.org/t/p/w300${path}`;
 };
+
+export type CastDataType = {
+  adult: boolean;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  order: number;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+};
+
+export type CrewDataType = {
+  adult: boolean;
+  credit_id: string;
+  department: string;
+  gender: number;
+  id: number;
+  job: string;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+};
+export type CreditDataType = {
+  id: number | null;
+  cast: CastDataType[];
+  crew: CrewDataType[];
+};
+
+export type SimilarDataType = {
+  adult: false;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+export type Genrestype = {
+  id: number;
+  name: string;
+};
+
+export type Componiestype = {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+};
+
+export type Countriestype = {
+  iso_3166_1: string;
+  name: string;
+};
+
+export type languagestype = {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+};
+export type SpecificDataType = {
+  adult?: boolean;
+  backdrop_path: string;
+  belongs_to_collection?: null;
+  budget?: number;
+  genres?: Genrestype[];
+  homepage?: string;
+  id?: number;
+  imdb_id?: string;
+  origin_country?: string[];
+  original_language?: string;
+  original_title?: string;
+  overview?: string;
+  popularity?: number;
+  poster_path: string;
+  production_companies?: Componiestype[];
+  production_countries?: Countriestype[];
+  release_date?: string;
+  revenue?: number;
+  runtime?: number;
+  spoken_languages?: languagestype[];
+  status?: string;
+  tagline?: string;
+  title?: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
+};
